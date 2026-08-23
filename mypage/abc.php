5cc1a8e2dbf665ae36deb2c5f31eb03d23804f9b
Rinchu <ritesshdawani@gamil.com>
2026-08-23 14:34:19 +0530

    project ai

---
+++ mypage/abc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            background color: #09e3f3;
+        }
+        .o1{
+            background color: red;
+        }
+        .o2{
+            background color: yellow;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "c1" >
+        <c1 class="o1">
+
+        </c1>
+        <c1 class="o2">
+            My First Page!
+        </c1>
+    </div>
+    
+</body>
+</html>
